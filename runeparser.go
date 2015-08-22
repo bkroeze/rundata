e4b8c0560d63f76d6be9f6b4407777db29c7d5f5
Bruce Kroeze <bkroeze@gmail.com>
2015-08-21 18:24:02 -0700

    Now parsing the whole file

--- runeparser.go
+++ runeparser.go
@@ -3,7 +3,8 @@ package runedata
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
+
+	"hillsorcerer.com/utils"
 )
 
 type Rune struct {
@@ -14,37 +15,65 @@ type Rune struct {
 	Divination  string
 }
 
-func RecordToRune(record string) (Rune, error) {
+func RecordToRune(record []string) (Rune, error) {
 	var rune Rune
-	fields := strings.Split(record, ",")
-	rune.Name = fields[0]
-	rune.Phonetic = fields[1]
-	rune.Anglo = fields[2]
-	rune.Traditional = fields[3]
-	rune.Divination = fields[4]
+
+	switch len(record) {
+	case 5:
+		rune.Divination = record[4]
+		fallthrough
+	case 4:
+		rune.Traditional = record[3]
+		fallthrough
+	case 3:
+		rune.Anglo = record[2]
+		fallthrough
+	case 2:
+		rune.Phonetic = record[1]
+		fallthrough
+	case 1:
+		rune.Name = record[0]
+	}
 	return rune, nil
+}
+
+func RecordsToRunes(raw string, skipFirst bool) ([]Rune, error) {
+
+	records, err := utils.SplitMultilineCSV(raw, skipFirst)
+	if err != nil {
+		return nil, err
+	}
+	runes := make([]Rune, len(records))
+
+	for ix := 0; ix < len(records); ix++ {
+		//fmt.Printf("%02d %s\n", ix, lines[ix])
+		rune, err := RecordToRune(records[ix])
+		if err != nil {
+			return runes, err
+		}
+		runes[ix] = rune
+	}
+	return runes, nil
+}
+
+func RunesFromFile(filename string) ([]Rune, error) {
+	raw, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	converted := string(raw[:])
+
+	return RecordsToRunes(converted, true)
 }
 
 func main() {
 
-	raw, err := ioutil.ReadFile("runes.csv")
+	runes, err := RunesFromFile("runes.csv")
 
 	if err != nil {
 		panic(err)
-	}
-
-	converted := string(raw[:])
-	var lines []string = strings.Split(converted, "\n")
-
-	var runes []Rune
-
-	for ix := 0; ix < len(lines); ix++ {
-		//fmt.Printf("%02d %s\n", ix, lines[ix])
-		rune, err := RecordToRune(lines[ix])
-		if err != nil {
-			panic(err)
-		}
-		runes = append(runes, rune)
 	}
 
 	fmt.Printf("%+v\n", runes)
--- runeparser_test.go
+++ runeparser_test.go
@@ -8,12 +8,61 @@ import (
 )
 
 var _ = Describe("Runeparser", func() {
+	var fehu Rune = Rune{Name: "fehu",
+		Phonetic:    "f",
+		Anglo:       "feoh",
+		Traditional: "cattle, wealth",
+		Divination:  "luck, wealth, starting out, creative spark"}
+
+	var uruz Rune = Rune{Name: "uruz",
+		Phonetic:    "u",
+		Anglo:       "ur",
+		Traditional: "aurochs",
+		Divination:  "Sap, growth, healing, qi, life energy, grounding, rooted center (reversed: blockage)"}
+
 	Context("Basic parse tests", func() {
+		It("Should return a Rune struct", func() {
+			record := []string{"fehu", "f", "feoh", "cattle, wealth", "luck, wealth, starting out, creative spark"}
+			Expect(RecordToRune(record)).Should(BeAssignableToTypeOf(Rune{}))
+		})
+
 		It("Should parse a simple line", func() {
-			record := "fehu,f,feoh,\"cattle, wealth\",\"luck, wealth, starting out, creative spark\""
-			rune, err := RecordToRune(record)
-			Expect(rune).NotTo(BeNil())
+			record := []string{"fehu", "f", "feoh", "cattle, wealth", "luck, wealth, starting out, creative spark"}
+			Expect(RecordToRune(record)).Should(BeEquivalentTo(fehu))
+
+		})
+	})
+
+	Context("Multiline parsing", func() {
+		It("Should parse multiple lines", func() {
+			lines := "fehu,f,feoh\nuruz,u,ur"
+			var shortfehu Rune = Rune{Name: "fehu",
+				Phonetic:    "f",
+				Anglo:       "feoh",
+				Traditional: "",
+				Divination:  ""}
+
+			var shorturuz Rune = Rune{Name: "uruz",
+				Phonetic:    "u",
+				Anglo:       "ur",
+				Traditional: "",
+				Divination:  ""}
+
+			runes, err := RecordsToRunes(lines, false)
 			Expect(err).To(BeNil())
+			Expect(runes).To(HaveLen(2))
+			Expect(runes[0]).Should(BeEquivalentTo(shortfehu))
+			Expect(runes[1]).Should(BeEquivalentTo(shorturuz))
+
+		})
+	})
+
+	Context("Parsing a file", func() {
+		It("Should parse the test file", func() {
+			runes, err := RunesFromFile("runes.csv")
+			Expect(err).To(BeNil())
+			Expect(runes[0]).To(BeEquivalentTo(fehu))
+			Expect(runes[1]).To(BeEquivalentTo(uruz))
 		})
 	})
 })
